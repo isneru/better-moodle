f08657c7fea86924639448c0fae81029942deea9
Diogo Nogueira <diogoclanogueira@gmail.com>
2025-11-20 12:52:57 +0000

    .

--- index.ts
+++ index.ts
@@ -3,18 +3,22 @@ import { setupRoutes } from './src/routes/index.ts'
 
 const app = fastify()
 
-await setupRoutes(app)
+// MOVED setupRoutes from here
 
-const PORT = isNaN(Number(process.env.PORT)) ? 3000 : Number(process.env.PORT)
+const PORT = isNaN(Number(process.env.PORT)) ? 3002 : Number(process.env.PORT)
 
 const start = async () => {
-	try {
-		await app.listen({ port: PORT })
-		console.log(`Better Moodle available at http://localhost:${PORT}`)
-	} catch (err) {
-		app.log.error(err)
-		process.exit(1)
-	}
+    try {
+        // MOVED setupRoutes TO HERE:
+        await setupRoutes(app) 
+
+        await app.listen({ port: PORT })
+        console.log(`Better Moodle available at http://localhost:${PORT}`)
+    } catch (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
 }
 
+// Call start() WITHOUT await
 start()
